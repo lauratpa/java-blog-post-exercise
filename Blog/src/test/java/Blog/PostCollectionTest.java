0998c8dedfa46abc0427f942eb3b56c13b2f28f1
Laura Paakkinen <la.paakkinen@gmail.com>
2020-09-23 20:35:01 +0200

    Use the original java 8 version and commment the nonpassing tests

--- Blog/src/test/java/Blog/PostCollectionTest.java
+++ Blog/src/test/java/Blog/PostCollectionTest.java
@@ -37,88 +37,89 @@ public class PostCollectionTest {
         assertEquals(expectedTitles, titles);
     }
 
-    @Ignore("methods for adding comments to be implemented")
-    @Test
-    public void canAddCommentsToPosts() {
-        // Arrange
-        TextPost textPost = new TextPost(textTitle, contents);
-        PhotoPost photoPost = new PhotoPost(photoTitle, photoUrl);
-        String firstComment = "So deep...";
-        String secondComment = "Beautiful!";
+    // @Ignore("methods for adding comments to be implemented")
+    // @Test
+    // public void canAddCommentsToPosts() {
+    // // Arrange
+    // TextPost textPost = new TextPost(textTitle, contents);
+    // PhotoPost photoPost = new PhotoPost(photoTitle, photoUrl);
+    // String firstComment = "So deep...";
+    // String secondComment = "Beautiful!";
 
-        // Act
-        textPost.addComment(firstComment);
-        photoPost.addComment(secondComment);
+    // // Act
+    // textPost.addComment(firstComment);
+    // photoPost.addComment(secondComment);
 
-        // Assert
-        assertEquals(firstComment, textPost.getComments().get(0));
-        assertEquals(secondComment, photoPost.getComments().get(0));
-    }
+    // // Assert
+    // assertEquals(firstComment, textPost.getComments().get(0));
+    // assertEquals(secondComment, photoPost.getComments().get(0));
+    // }
 
-    @Ignore("methods for adding reviews to be implemented")
-    @Test
-    public void canAddReviewsToPosts() {
-        // Arrange
-        TextPost textPost = new TextPost(textTitle, contents);
-        PhotoPost photoPost = new PhotoPost(photoTitle, photoUrl);
+    // @Ignore("methods for adding reviews to be implemented")
+    // @Test
+    // public void canAddReviewsToPosts() {
+    // // Arrange
+    // TextPost textPost = new TextPost(textTitle, contents);
+    // PhotoPost photoPost = new PhotoPost(photoTitle, photoUrl);
 
-        Integer firstReview = 1;
-        Integer secondReview = 3;
+    // Integer firstReview = 1;
+    // Integer secondReview = 3;
 
-        // Act
-        textPost.addReview(firstReview);
-        photoPost.addReview(secondReview);
+    // // Act
+    // textPost.addReview(firstReview);
+    // photoPost.addReview(secondReview);
 
-        // Assert
-        assertEquals(firstReview, textPost.getReviews().get(0));
-        assertEquals(secondReview, photoPost.getReviews().get(0));
-    }
+    // // Assert
+    // assertEquals(firstReview, textPost.getReviews().get(0));
+    // assertEquals(secondReview, photoPost.getReviews().get(0));
+    // }
 
-    @Ignore("create video post")
-    @Test
-    public void canCreateVideoPosts() {
-        // Arrange
-        VideoPost videoPost = new VideoPost(textTitle, videoUrl);
-        String comment = "Needs more cats";
+    // @Ignore("create video post")
+    // @Test
+    // public void canCreateVideoPosts() {
+    // // Arrange
+    // VideoPost videoPost = new VideoPost(textTitle, videoUrl);
+    // String comment = "Needs more cats";
 
-        // Act
-        videoPost.addComment(comment);
+    // // Act
+    // videoPost.addComment(comment);
 
-        // Assert
-        assertEquals(comment, videoPost.getComments().get(0));
-    }
+    // // Assert
+    // assertEquals(comment, videoPost.getComments().get(0));
+    // }
 
-    @Ignore("add video post")
-    @Test
-    public void canAddVideoPostsToCollection[() {
-        // Arrange
-        PostCollection posts = new PostCollection();
+    // @Ignore("add video post")
+    // @Test
+    // public void canAddVideoPostsToCollection[() {
+    // // Arrange
+    // PostCollection posts = new PostCollection();
 
-        ArrayList<String> expectedTitles = new ArrayList<String>(Arrays.asList(textTitle, videoTitle));
+    // ArrayList<String> expectedTitles = new
+    // ArrayList<String>(Arrays.asList(textTitle, videoTitle));
 
-        // Act
-        posts.addVideoPost(videoTitle, videoUrl);
+    // // Act
+    // posts.addVideoPost(videoTitle, videoUrl);
 
-        // Assert
-        ArrayList<String> titles = new ArrayList<>();
-        for (Postable post : posts.getAll()) {
-            titles.add(post.getTitle());
-        }
+    // // Assert
+    // ArrayList<String> titles = new ArrayList<>();
+    // for (Postable post : posts.getAll()) {
+    // titles.add(post.getTitle());
+    // }
 
-        assertEquals(videoTitle, titles.get(0));
-    }
+    // assertEquals(videoTitle, titles.get(0));
+    // }
 
-    @Ignore("can add text reviews to post")
-    @Test
-    public void canAddReviewsToVideoPosts() {
-        // Arrange
-        VideoPost videoPost = new VideoPost(textTitle, videoUrl);
-        String review = "Nice";
+    // @Ignore("can add text reviews to post")
+    // @Test
+    // public void canAddReviewsToVideoPosts() {
+    // // Arrange
+    // VideoPost videoPost = new VideoPost(textTitle, videoUrl);
+    // String review = "Nice";
 
-        // Act
-        videoPost.addReview(review);
+    // // Act
+    // videoPost.addReview(review);
 
-        // Assert
-        assertEquals(review, videoPost.getReview().get(0));
-    }
+    // // Assert
+    // assertEquals(review, videoPost.getReview().get(0));
+    // }
 }
